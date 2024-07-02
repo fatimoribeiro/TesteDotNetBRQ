faefc2ecdd859bf8e5e05092177cc44b69244c13
Fatimo Ribeiro <fatimoribeiro@gmail.com>
2024-07-02 16:39:41 -0300

    Inclusão dos imports do angular material no arquivo app.module.ts

--- frontend/FronEndAngularTodoBRQ/src/app/app.module.ts
+++ frontend/FronEndAngularTodoBRQ/src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ExcluirComponent } from './components/excluir/excluir.component';
+
+/* Angular Material */
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -14,7 +26,16 @@ import { ExcluirComponent } from './components/excluir/excluir.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
