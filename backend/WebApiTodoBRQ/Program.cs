b59da6976adf9a1ba3705cabd5656eb6c1e66007
Fatimo Ribeiro <fatimoribeiro@gmail.com>
2024-07-02 14:24:59 -0300

    Inclusão de AddCors e UseCors em Programs.cs para evitar problemas de acesso pelo frontend

--- backend/WebApiTodoBRQ/Program.cs
+++ backend/WebApiTodoBRQ/Program.cs
@@ -15,6 +15,10 @@ builder.Services.AddScoped<ITarefaInterface, TarefaService>();
 var connectionStr = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionStr));
 
+#region [CORS]
+builder.Services.AddCors();
+#endregion [CORS]
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +27,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+#region [Use of CORS]
+app.UseCors(c =>
+{
+    c.AllowAnyHeader();
+    c.AllowAnyMethod();
+    c.AllowAnyOrigin();
+});
+#endregion [Use of CORS]
 
 app.UseHttpsRedirection();
 
