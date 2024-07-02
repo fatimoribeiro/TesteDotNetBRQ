ef9411a2bd864b3d34634645292fe7bb3b5606e0
Fatimo Ribeiro <fatimoribeiro@gmail.com>
2024-07-02 15:59:16 -0300

    Inclusão das chamadas aos endpoints da api no arquivo tarefa.service.ts

--- frontend/FronEndAngularTodoBRQ/src/app/services/tarefa.service.ts
+++ frontend/FronEndAngularTodoBRQ/src/app/services/tarefa.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Tarefa } from '../models/Tarefas';
+import { Response } from '../models/Response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TarefaService {
+  private apiUrl = `${environment.ApiUrl}/funcionario`
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  CreateTarefas(tarefa: Tarefa) : Observable<Response<Tarefa[]>> {
+    return this.http.post<Response<Tarefa[]>>(this.apiUrl, tarefa);
+  }
+
+  GetTarefas() : Observable<Response<Tarefa[]>> {
+    return this.http.get<Response<Tarefa[]>>(this.apiUrl);
+  }
+
+  GetTarefasById(id: number) : Observable<Response<Tarefa>> {
+    return this.http.get<Response<Tarefa>>(`${this.apiUrl}/${id}`);
+  }
+
+  UpdateTarefas(tarefa: Tarefa) : Observable<Response<Tarefa[]>> {
+    return this.http.put<Response<Tarefa[]>>(this.apiUrl, tarefa);
+  }
+
+  DeleteTarefas(id: number) : Observable<Response<Tarefa[]>> {
+    return this.http.delete<Response<Tarefa[]>>(`${this.apiUrl}?id=${id}`);
+  }
 }
