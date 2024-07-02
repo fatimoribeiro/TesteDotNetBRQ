2be185f0435a13d60020ed3ebebc5c09a60d7bd4
Fatimo Ribeiro <fatimoribeiro@gmail.com>
2024-07-02 11:09:52 -0300

    Alteração do arquivo TarefaService.cs para inclusão das regras de negócios do projeto

--- backend/WebApiTodoBRQ/Service/TarefaService/TarefaService.cs
+++ backend/WebApiTodoBRQ/Service/TarefaService/TarefaService.cs
@@ -1,32 +1,159 @@
 ﻿using WebApiTodoBRQ.Models;
+using WebApiTodoBRQ.DataContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApiTodoBRQ.Service.TarefaService
 {
     public class TarefaService : ITarefaInterface
     {
-        public Task<ServiceResponse<List<TarefaModel>>> CreateTarefas(TarefaModel novaTarefa)
+        private readonly ApplicationDbContext _context;
+
+        public TarefaService(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task<ServiceResponse<List<TarefaModel>>> DeleteTarefas(int id)
+        public async Task<ServiceResponse<List<TarefaModel>>> CreateTarefas(TarefaModel novaTarefa)
         {
-            throw new NotImplementedException();
+            ServiceResponse<List<TarefaModel>> serviceResponse = new ServiceResponse<List<TarefaModel>>();
+
+            try
+            {
+                if (novaTarefa == null)
+                {
+                    serviceResponse.Dados = null;
+                    serviceResponse.Mensagem = "Informar dados.";
+                    serviceResponse.Sucesso = false;
+
+                    return serviceResponse;
+                }
+
+                _context.Add(novaTarefa);
+                await _context.SaveChangesAsync();
+
+                serviceResponse.Dados = await _context.Tarefas.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Mensagem = ex.Message;
+                serviceResponse.Sucesso = false;
+            }
+
+            return serviceResponse;
         }
 
-        public Task<ServiceResponse<List<TarefaModel>>> GetTarefas()
+        public async Task<ServiceResponse<List<TarefaModel>>> GetTarefas()
         {
-            throw new NotImplementedException();
+            ServiceResponse<List<TarefaModel>> serviceResponse = new ServiceResponse<List<TarefaModel>>();
+
+            try
+            {
+                serviceResponse.Dados = await _context.Tarefas.ToListAsync();
+
+                if (serviceResponse.Dados.Count == 0)
+                {
+                    serviceResponse.Mensagem = "Nenhum dado encontrado.";
+                }
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Mensagem = ex.Message;
+                serviceResponse.Sucesso = false;
+            }
+
+            return serviceResponse;
         }
 
-        public Task<ServiceResponse<TarefaModel>> GetTarefasById(int id)
+        public async Task<ServiceResponse<TarefaModel>> GetTarefasById(int id)
         {
-            throw new NotImplementedException();
+            ServiceResponse<TarefaModel> serviceResponse = new ServiceResponse<TarefaModel>();
+
+            try
+            {
+                var tarefa = await _context.Tarefas.FirstOrDefaultAsync(x => x.Id == id);
+
+                if (tarefa == null)
+                {
+                    serviceResponse.Dados = null;
+                    serviceResponse.Mensagem = "Tarefa não localizada.";
+                    serviceResponse.Sucesso = false;
+                }
+                else
+                {
+                    serviceResponse.Dados = tarefa;
+                }
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Mensagem = ex.Message;
+                serviceResponse.Sucesso = false;
+            }
+
+            return serviceResponse;
         }
 
-        public Task<ServiceResponse<List<TarefaModel>>> UpdateTarefas(TarefaModel editadaTarefa)
+        public async Task<ServiceResponse<List<TarefaModel>>> UpdateTarefas(TarefaModel editadaTarefa)
         {
-            throw new NotImplementedException();
+            ServiceResponse<List<TarefaModel>> serviceResponse = new ServiceResponse<List<TarefaModel>>();
+
+            try
+            {
+                var tarefa = await _context.Tarefas.AsNoTracking().FirstOrDefaultAsync(x => x.Id == editadaTarefa.Id);
+
+                if (tarefa == null)
+                {
+                    serviceResponse.Dados = null;
+                    serviceResponse.Mensagem = "Tarefa não localizada.";
+                    serviceResponse.Sucesso = false;
+                }
+                else
+                {
+                    tarefa.DataDeAlteracao = DateTime.Now.ToLocalTime();
+
+                    _context.Tarefas.Update(editadaTarefa);
+                    await _context.SaveChangesAsync();
+                }
+
+                serviceResponse.Dados = await _context.Tarefas.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Mensagem = ex.Message;
+                serviceResponse.Sucesso = false;
+            }
+
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<List<TarefaModel>>> DeleteTarefas(int id)
+        {
+            ServiceResponse<List<TarefaModel>> serviceResponse = new ServiceResponse<List<TarefaModel>>();
+
+            try
+            {
+                var funcionario = await _context.Tarefas.FirstOrDefaultAsync(x => x.Id == id);
+
+                if (funcionario == null)
+                {
+                    serviceResponse.Dados = null;
+                    serviceResponse.Mensagem = "Tarefa não localizada.";
+                    serviceResponse.Sucesso = false;
+                }
+                else
+                {
+                    _context.Tarefas.Remove(funcionario);
+                    await _context.SaveChangesAsync();
+
+                    serviceResponse.Dados = await _context.Tarefas.ToListAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Mensagem = ex.Message;
+                serviceResponse.Sucesso = false;
+            }
+
+            return serviceResponse;
         }
     }
 }
