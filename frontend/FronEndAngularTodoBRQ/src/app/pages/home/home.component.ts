1868c239343c3c7bfec42e40e713fb4439fe71b0
Fatimo Ribeiro <fatimoribeiro@gmail.com>
2024-07-02 16:16:18 -0300

    Criação do Html da página Home e funcionalidades dos componentes da página

--- frontend/FronEndAngularTodoBRQ/src/app/pages/home/home.component.ts
+++ frontend/FronEndAngularTodoBRQ/src/app/pages/home/home.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tarefa } from 'src/app/models/Tarefas';
+import { TarefaService } from 'src/app/services/tarefa.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  tarefas: Tarefa[] = [];
+  tarefasGeral: Tarefa[] = [];
+  columnsToDisplay = ['Titulo', 'Status', 'Actions', 'Excluir'];
+
+  constructor(private tarefaService: TarefaService) {}
+
+  ngOnInit(): void {
+    this.tarefaService.GetTarefas().subscribe(data => {      
+      const dados = data.dados;
+      
+      dados.map((item) => {
+        item.dataDeCriacao = new Date(item.dataDeCriacao!).toLocaleDateString("pt-BR");
+        item.dataDeAlteracao = new Date(item.dataDeAlteracao!).toLocaleDateString("pt-BR");
+      });
+
+      this.tarefas = data.dados;
+      this.tarefasGeral = data.dados;
+    });  
+  }
+
+  search(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value.toLowerCase();
+    
+    this.tarefas = this.tarefasGeral.filter(tarefa => {
+      return tarefa.titulo.toLocaleLowerCase().includes(value);
+    });
+  }
+
+  OpenDialog(id: number) {
+  }
 }
