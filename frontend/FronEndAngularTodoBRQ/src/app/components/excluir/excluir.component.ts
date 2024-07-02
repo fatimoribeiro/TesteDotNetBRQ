a417a0c354ff6e328166c14446fc4292d0d1b02c
Fatimo Ribeiro <fatimoribeiro@gmail.com>
2024-07-02 17:07:17 -0300

    Criação do Html do componente 'excluir' e suas funcionalidades no arquivo excluir.components.ts

--- frontend/FronEndAngularTodoBRQ/src/app/components/excluir/excluir.component.ts
+++ frontend/FronEndAngularTodoBRQ/src/app/components/excluir/excluir.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { Tarefa } from 'src/app/models/Tarefas';
+import { TarefaService } from 'src/app/services/tarefa.service';
 
 @Component({
   selector: 'app-excluir',
   templateUrl: './excluir.component.html',
   styleUrls: ['./excluir.component.css']
 })
-export class ExcluirComponent {
+export class ExcluirComponent implements OnInit {
 
+  inputdata: any;
+  tarefa!: Tarefa;
+
+  constructor (
+    private tarefaService: TarefaService, 
+    private router: Router,
+    private ref: MatDialogRef<ExcluirComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
+  
+  ngOnInit(): void {
+    this.inputdata = this.data;
+
+    this.tarefaService.GetTarefasById(this.inputdata.id).subscribe((data) => {
+      this.tarefa = data.dados;
+    });
+  }
+
+  Excluir() {
+    this.tarefaService.DeleteTarefas(this.inputdata.id).subscribe((data) => {
+      this.ref.close();
+      window.location.reload();
+    });
+  }
+
+  Cancelar() {
+    this.ref.close();
+  }
 }
