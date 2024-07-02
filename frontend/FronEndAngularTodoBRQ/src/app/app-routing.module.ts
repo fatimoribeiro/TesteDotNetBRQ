5e258798b7417c37322b0667c2008db249a9beec
Fatimo Ribeiro <fatimoribeiro@gmail.com>
2024-07-02 19:11:19 -0300

    Alteração do arquivo app-routing.module.ts para inclusão das rotas para as páginas de Cadastro e Editar

--- frontend/FronEndAngularTodoBRQ/src/app/app-routing.module.ts
+++ frontend/FronEndAngularTodoBRQ/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { EditarComponent } from './pages/editar/editar.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'editar/:id', component: EditarComponent}
 ];
 
 @NgModule({
