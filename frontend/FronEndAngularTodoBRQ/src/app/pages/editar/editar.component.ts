21c063abbaaf73dca43ec77da585ab05d1292acb
Fatimo Ribeiro <fatimoribeiro@gmail.com>
2024-07-03 09:12:07 -0300

    Criação do Html da página Editar e suas funcionalidades no arquivo editar-component.ts

--- frontend/FronEndAngularTodoBRQ/src/app/pages/editar/editar.component.ts
+++ frontend/FronEndAngularTodoBRQ/src/app/pages/editar/editar.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tarefa } from 'src/app/models/Tarefas';
+import { TarefaService } from 'src/app/services/tarefa.service';
 
 @Component({
   selector: 'app-editar',
   templateUrl: './editar.component.html',
   styleUrls: ['./editar.component.css']
 })
-export class EditarComponent {
+export class EditarComponent implements OnInit {
+  btnAcao: string = "Editar";
+  lblTitulo: string = "Editar Tarefa";
+  tarefa!: Tarefa;
 
+  constructor(private tarefaService: TarefaService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.tarefaService.GetTarefasById(id).subscribe((data) => {
+      this.tarefa = data.dados;
+    })
+  }
+
+  editarTarefa(tarefa: Tarefa) {
+    this.tarefaService.UpdateTarefas(tarefa).subscribe((data) => {
+      this.router.navigate(['/']);
+    });
+  }
 }
